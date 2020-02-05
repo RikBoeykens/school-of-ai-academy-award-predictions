ad53cbb8169fce3bd0968e89599fa3ceee233428
Rik Boeykens <rikboeykens@gmail.com>
2020-02-05 18:33:16 +0100

    init

---
+++ academy_award_predictor_constants.py
@@ -0,0 +1,16 @@
+# File constants
+NOMINATION_FILE_DATA = 'data'
+NOMINATION_FILE_PREDICT = 'predict'
+NOMINATION_FILE_PREFIX = '.csv'
+
+# Feature constants
+NOMINATIONS_PREFIX = 'Nominations'
+FILM_PREFIX = 'Film'
+PERSON_PREFIX = 'Person'
+CREW_PREFIX= 'Crew'
+COLOR_PREFIX = 'Color'
+ASPECT_RATIO_PREFIX = 'AspectRatio'
+CERTIFICATION_PREFIX = 'Certfication'
+GENRE_PREFIX = 'Genre'
+KEYWORD_PREFIX = 'Keyword'
+REVIEW_PREFIX = 'Review'
